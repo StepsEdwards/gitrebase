abb89f3cda6f166135f53484b0d8125c30b8593d
Stephens Jean-Jacques <stepsedwards@gmail.com>
2018-03-01 22:37:22 -0500

    added myFunction

--- app.js
+++ app.js
@@ -8,3 +8,15 @@ for(i = 0; i < 100; i++){
         numArr.push(i);
 } 
 console.log(numArr);  
+
+// myFunction
+function myFunction(count){
+    const countArr = [];
+    for(let i = count; count <= 10; count++){
+        i *= 6;
+        countArr.push(i);
+    }
+    console.log(countArr);
+}
+
+myFunction(6);
