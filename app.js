3b5799f2237d0b682149fb4c078acb51a0d2b454
Stephens Jean-Jacques <stepsedwards@gmail.com>
2018-03-01 22:18:23 -0500

    multiples of 13

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 console.log('Hello World!')
 console.log('Hello World Again!!!');
+
+// Multiples of 13
+const numArr = [];
+for(i = 0; i < 100; i++){
+    if(i % 13 == 0)
+        numArr.push(i);
+} 
+console.log(numArr);  
